be2bd36d0cfaf6dccf9b209a6ae536ba3c009e04
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2024-02-18 13:28:12 -0600

    Worked in file 10,11

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- shows in at least one genre, alphabetical order
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM (tv_show_genres
+    INNER JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id)
+    ORDER BY tv_shows.title, tv_shows_genres.genre_id ASC;
---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- show tv shows, in alphabetical order with genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM (tv_show_genres
+    RIGHT OUTER JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id or NULL)
+    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
