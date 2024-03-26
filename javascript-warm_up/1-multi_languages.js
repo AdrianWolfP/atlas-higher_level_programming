dd1635b0c8a4587edaf22c913b03b6717eaef7de
AdrianWolfP <wolfy52032@gmail.com>
2024-03-26 12:26:56 -0500

    Worked on this files and adding them in

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+
+if (argv.length < 3) {
+    console.log('C is fun');
+    console.log('Python is cool');
+    console.log('JavaScript is amazing');
+} else if (argv.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Argument found');
+}
---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const argv = prosse.argv;
+
+console.log(argv[2] + ' is ' + argv[3]);
