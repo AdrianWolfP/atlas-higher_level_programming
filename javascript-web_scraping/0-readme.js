b3b0ff6a77e145bda07b684667711d741b99c735
AdrianWolfP <Wolfy52032@gmail.com>
2024-04-01 15:05:02 -0500

    semistandard fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const ARGV = process.argv;
 
 fs.readFile(ARGV[2], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+  }
+  console.log(data);
 });
